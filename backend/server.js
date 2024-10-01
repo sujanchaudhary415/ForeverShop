c7b85ce27daa86cc07bd3c86a2cb19166bbc60fc
sujanchaudhary415 <108916550+sujanchaudhary415@users.noreply.github.com>
2024-10-01 08:39:16 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectCloudinary()
 // Middlewares
 app.use(express.json())
 app.use(cors({
-    origin: ['https://forever-frontend-orcin.vercel.app', 'http://localhost:5173']
+    origin: ['https://forever-frontend-orcin.vercel.app']
 }))
 
 
